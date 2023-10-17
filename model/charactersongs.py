09d5397ba9a873d7521b88cf870543b0d8e6f316
SrijDude3416 <srij.0504@gmail.com>
2023-10-17 10:02:08 -0700

    singer name stored in db

--- model/charactersongs.py
+++ model/charactersongs.py
@@ -5,16 +5,18 @@ class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # Define a primary key column
     character = db.Column(db.String, nullable=False)
     song_name = db.Column(db.String, nullable=False)
+    singer = db.Column(db.String, nullable=False)
     era = db.Column(db.String, nullable=False)
-    def __init__(self, character, song_name, era):
+    def __init__(self, character, song_name, singer, era):
         self.character = character
         self.song_name = song_name
+        self.singer = singer
         self.era = era
     def to_dict(self):
         return {"character": self.character, "song_name": self.song_name, "era": self.era}
 def initSongs():
     # You can keep the rest of your code as is
-    song1 = Song(character="Walter White", song_name="Song 1", era="Innocent"); db.session.add(song1)#replace with real data
-    song2 = Song(character="Walter White", song_name="Song 2", era="Grunge"); db.session.add(song2)
-    song3 = Song(character="Walter White", song_name="Song 3", era="Prog Rock"); db.session.add(song3)
+    song1 = Song(character="Walter White", song_name="Changes", singer="David Bowie", era="Innocent"); db.session.add(song1)#replace with real data
+    song2 = Song(character="Walter White", song_name="Back in Black", singer="AC/DC", era="Grunge"); db.session.add(song2)
+    song3 = Song(character="Walter White", song_name="Baby Blue", singer="Badfinger", era="Prog Rock"); db.session.add(song3)
     db.session.commit()
