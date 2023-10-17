5c4b3a8e9ce4507f509c776c8a36dd2cac96cc1a
advikg <advik.sd@gmail.com>
2023-10-16 21:26:01 -0700

    Character Songs DB showing up w/main.py!!

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
-
+from model.charactersongs import initSongs
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
+from api.charactersong import song_api
 
 
 # setup App pages
@@ -29,6 +30,7 @@ app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
+app.register_blueprint(song_api)
 app.register_blueprint(app_projects) # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
@@ -49,6 +51,7 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
+    initSongs()
 
 # this runs the application on the development server
 if __name__ == "__main__":
--- model/charactersongs.py
+++ model/charactersongs.py
@@ -3,7 +3,7 @@ from __init__ import db
 class Song(db.Model):
     __tablename__ = "Song"
     id = db.Column(db.Integer, primary_key=True)  # Define a primary key column
-    character = db.Column(db.Stri+ng, nullable=False)
+    character = db.Column(db.String, nullable=False)
     song_name = db.Column(db.String, nullable=False)
     era = db.Column(db.String, nullable=False)
     def __init__(self, character, song_name, era):
@@ -12,7 +12,7 @@ class Song(db.Model):
         self.era = era
     def to_dict(self):
         return {"character": self.character, "song_name": self.song_name, "era": self.era}
-def init_Songs():
+def initSongs():
     # You can keep the rest of your code as is
     song1 = Song(character="Walter White", song_name="Song 1", era="Innocent"); db.session.add(song1)#replace with real data
     song2 = Song(character="Walter White", song_name="Song 2", era="Grunge"); db.session.add(song2)
