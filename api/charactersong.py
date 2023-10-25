4932089cb830747d3f9236fd4dbd86db711424d3
advikg <advik.sd@gmail.com>
2023-10-24 22:28:51 -0700

    FULLY TESTED locally song api - create and get

--- api/charactersong.py
+++ api/charactersong.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_restful import Api, Resource, reqparse
 from __init__ import db
 from model.charactersongs import Song  # Import the song model
+import requests
 
 # Create a Blueprint for the song API
 song_api = Blueprint('song_api', __name__, url_prefix='/api/song')
@@ -9,86 +10,39 @@ song_api = Blueprint('song_api', __name__, url_prefix='/api/song')
 # Create the API instance
 api = Api(song_api)
 
-class songAPI(Resource):
-    def get(self, id):
-        # Retrieve a single song by its ID from the database
-        song = Song.query.get(id)
+class SongAPI:
+    # not implemented
+    class _Create(Resource):
+        def post(self):
+            # get request body
+            body = request.get_json()
+            # get variables
+            character = body.get('character')
+            song_name = body.get('song_name')
+            artist = body.get('artist')
+            genre = body.get('genre')
 
-        if song:
-            return song.to_dict(), 200
-        else:
-            return {"message": "song not found"}, 404
-
-    def put(self, id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("character", type=str)
-        parser.add_argument("song_name", type=str)
-        parser.add_argument("artist", type=str)
-        parser.add_argument("genre", type=str)
-        args = parser.parse_args()
-
-        try:
-            song = db.session.query(song).get(id)
+            # Set up Song class object
+            song_obj = Song(character=character, song_name=song_name, artist=artist, genre=genre)
+            
+            ''' #2: Key Code block to add song to database '''
+            # create song in database
+            song = song_obj.create()
+            # success returns json of song
             if song:
-                if args["song_name"] is not None:
-                    song.song_name = args["song_name"]
-                if args["character"] is not None:
-                    song.character = args["character"]
-                if args["genre"] is not None:
-                    song.genre = args["genre"]
-                if args["artist"] is not None:
-                    song.artist = args["artist"]
-                db.session.commit()
-                return song.to_dict(), 200
-            else:
-                return {"message": "song not found"}, 404
-        except Exception as exception:
-            db.session.rollback()
-            return {"message": f"Error: {exception}"}, 500
+                return jsonify(song.read())
+            # failure returns error
+            return {'message': f'Invalid input, correct fields should be character, song_name, artist, and genre'}, 400
 
-    def delete(self, id):
-        try:
-            song = db.session.query(song).get(id)
-            if song:
-                db.session.delete(song)
-                db.session.commit()
-                return song.to_dict()
-            else:
-                return {"message": "song not found"}, 404
-        except Exception as exception:
-            db.session.rollback()
-            return {"message": f"Error: {exception}"}, 500
-
-# Add the songAPI resource to the /api/song/<int:id> endpoint
-api.add_resource(songAPI, "/<int:id>")
-
-class songListAPI(Resource):
-    def get(self):
+            
+    class _Read(Resource):
+        def get(self):
         # Retrieve all songs from the database
-        songs = Song.query.all()
-
-        # Prepare the data in JSON format
-        json_ready = [song.to_dict() for song in songs]
-
+            songs = Song.query.all()
+            json_ready = [song.to_dict() for song in songs]
         # Return the JSON response
-        return jsonify(json_ready)
+            return jsonify(json_ready)
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("song_name", required=True, type=str)
-        parser.add_argument("character", required=True, type=str)
-        parser.add_argument("genre", required=True, type=str) 
-        parser.add_argument("artist", required=True, type=str) 
-        args = parser.parse_args()
-        song = song(args["song_name"], args["character"], args["genre"], args["artist"])
-
-        try:
-            db.session.add(song)
-            db.session.commit()
-            return song.to_dict(), 201
-        except Exception as exception:
-            db.session.rollback()
-            return {"message": f"Error: {exception}"}, 500
-
-# Add the songListAPI resource to the /api/song endpoint
-api.add_resource(songListAPI, "/")
+    # building RESTapi resources/interfaces, these routes are added to Web Server
+    api.add_resource(_Create, '/create')
+    api.add_resource(_Read, '/')
